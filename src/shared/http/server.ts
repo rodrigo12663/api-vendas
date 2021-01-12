46c913a795655ca252baccc16652cf985edb8cef
Rodrigo Vieira <rodrigo202145@gmail.com>
2021-01-12 09:29:01 -0300

    configuration finish the classError

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express'
+import 'reflect-metadata'
 import AppError from '@shared/errors/AppError'
 import cors from 'cors'
 import routes from './routes'
