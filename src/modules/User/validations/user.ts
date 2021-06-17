f333da08e1a249f41e27e18c8806a76cedf1bc57
rodrigo <rodrigovieiranunes23@outlook.com>
2021-06-17 16:03:17 -0300

    Revert "added modules user"
    This reverts commit 416820caa93a662adc0aa3ef733d6a88a70e9ac4.

--- src/modules/User/validations/user.ts
+++ src/modules/User/validations/user.ts
@@ -2,8 +2,8 @@ import * as Yup from 'yup'
 
 const user = Yup.object().shape({
   name: Yup.string().required(),
-  email: Yup.string().email().required(),
-  password: Yup.string().required()
+  email: Yup.string().required(),
+  password: Yup.string()
 
 })
 
